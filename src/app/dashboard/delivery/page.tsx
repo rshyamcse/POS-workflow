dc282767449b7582cd59eab21e5476776f80f5c7
rshyamcse <rshyamcse@gmail.com>
2026-07-14 14:39:52 +0530

    feat: remove obsolete API module and associated configuration files

--- src/app/dashboard/delivery/page.tsx
+++
@@ -1,3 +0,0 @@
-'use client';
-
-export default function Page() { return (<div className="flex items-center justify-center h-full text-2xl font-bold text-muted-foreground">DELIVERY MODULE (Under Construction)</div>); }
